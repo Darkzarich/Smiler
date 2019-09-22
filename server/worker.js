7c91a1f899df0a9d3b34241c20ef3b4194ee41c1
Darkzarich <darkzarichv2@gmail.com>
2019-09-22 04:09:11 +0300

    server serve pics heroku fix 5 clean the mess

--- server/worker.js
+++ server/worker.js
@@ -29,10 +29,8 @@ app.use(
 app.use(router);
 
 if (config.IS_PRODUCTION) {
-  console.log('!!!!!!!!!!!!!1');
   app.use('/app/uploads', express.static(path.join(__dirname, 'uploads')));
 } else {
-  console.log('!!!!!!!!!!!!!2');
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 }
 
