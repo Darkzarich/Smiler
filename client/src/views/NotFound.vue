639b3502a8368150a137bab0437a3d38cf1ec2b0
Darkzarich <darkzarichv2@gmail.com>
2019-09-29 17:23:31 +0300

    front: better not found, back button from not found, change background color

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,15 +1,62 @@
 <template>
-  <div>
-    Not found
+  <div class="not-found">
+
+    <div class="not-found__404">
+      404
+    </div>
+    <div class="not-found__info">
+      Not found
+    </div>
+    <div class="not-found__button-container">
+      <button-element
+        :callback="goBack"
+      >
+        Go back
+      </button-element>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
 
+import buttonElement from '@/components/BasicElements/ButtonElement';
+
+export default {
+  components: {
+    buttonElement,
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-2);
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import '@/styles/colors';
+@import '@/styles/mixins';
 
+  .not-found {
+    @include widget();
+    @include flex-col();
+    align-items: center;
+    &__404 {
+      color: $main-text;
+      font-weight: bold;
+      font-size: 4rem;
+      margin-bottom: 1rem;
+    }
+
+    &__info {
+      color: $main-text;
+      font-size: 3rem;
+      margin-bottom: 1rem;
+    }
+
+    &__button-container {
+      width: 50%;
+    }
+  }
 </style>
