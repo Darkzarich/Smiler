d06f3192fe37f56a274d2de115325e38d62bdd17
Darkzarich <darkzarichv2@gmail.com>
2023-12-28 14:53:39 +0300

    frontend add base for Playwright tests

---
+++ client/playwright.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+const BASE_URL = 'http://localhost:4173';
+
+module.exports = defineConfig({
+  testDir: './tests/e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: BASE_URL,
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run serve',
+    url: BASE_URL,
+    reuseExistingServer: !process.env.CI,
+  },
+});
