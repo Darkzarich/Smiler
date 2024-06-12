a11f8eadfbdbe873cf0bca0391d2643cd810284e
Darkzarich <darkzarichv2@gmail.com>
2024-06-13 02:45:28 +0300

    backend change default auth error message

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     if (req.session.userId) {
       next();
     } else {
-      const error = new Error('Auth is required for this operation. Please log in.');
+      const error = new Error('Auth is required for this operation. Please sign in.');
       res.status(401).json({
         error: {
           message: error.message,
