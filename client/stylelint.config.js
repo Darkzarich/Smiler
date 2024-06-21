0f0edb9ac23eb014cc31dea936b15125e18f69c6
Darkzarich <darkzarichv2@gmail.com>
2024-06-21 21:53:13 +0300

    frontend install my stylelint two dash bem rule as an npm package

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -6,18 +6,11 @@ module.exports = {
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
     'stylelint-prettier/recommended',
+    'stylelint-config-two-dash-bem',
   ],
   rules: {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
-    'selector-class-pattern': [
-      '^[a-z][-a-z0-9]+(__[-a-z0-9]+)?(--[a-z0-9]+)?$',
-      {
-        message: (selector) =>
-          `Selector class ${selector} violates BEM Convention`,
-        resolveNestedSelectors: true,
-      },
-    ],
     // According to Concentric CSS
     'order/properties-order': [
       [
