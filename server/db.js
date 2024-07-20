791c43eb89a87bdf44906ef3e7119c65c10c1180
Darkzarich <darkzarichv2@gmail.com>
2024-07-21 01:45:52 +0300

    backend catch db connection error, add more db events logs

--- server/db.js
+++ server/db.js
@@ -1,22 +1,36 @@
 const mongoose = require('mongoose');
-const logger = require('./src/config/winston');
+const logger = require('./src/config/logger');
 const config = require('./src/config/config');
 
-mongoose.connect(config.DB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(config.DB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((error) => {
+    logger.error(error);
+  });
 
 const db = mongoose.connection;
 
-db.on('error', global.console.error.bind(console, 'connection error:')).once(
-  'open',
-  () => {
-    logger.info(
-      `${process.pid} [pid]: Successfully connected to MongoDB database`,
-    );
-  },
-);
+db.on('error', (error) => {
+  logger.error(error);
+});
+
+db.once('connected', () => {
+  logger.info(
+    `[pid: ${process.pid}]: Successfully connected to MongoDB database`,
+  );
+});
+
+db.once('disconnected', () => {
+  logger.warn(`[pid: ${process.pid}]: Disconnected from MongoDB database`);
+});
+
+db.on('reconnected', () => {
+  logger.info(`[pid: ${process.pid}]: Reconnected to MongoDB database`);
+});
 
 module.exports = db;
