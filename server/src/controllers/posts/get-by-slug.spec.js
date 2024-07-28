8809f0d7559421c175666f851e6a56c221078c67
Darkzarich <darkzarichv2@gmail.com>
2024-07-28 20:12:24 +0300

    backend refactor tests for getting a post by its slug in accordance to the new test setup

--- server/src/controllers/posts/get-by-slug.spec.js
+++ server/src/controllers/posts/get-by-slug.spec.js
@@ -1,32 +1,67 @@
 const request = require('supertest');
-const main = require('../../../worker');
+const { startApp } = require('../../../worker');
+const { connectDB } = require('../../../db');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
 
 let app;
-let server;
 let db;
 
 beforeAll(async () => {
-  const resolvedMain = await main();
+  db = await connectDB();
 
-  app = resolvedMain.app;
-  server = resolvedMain.server;
-  db = resolvedMain.db;
+  const resolvedApp = await startApp({ db });
+
+  app = resolvedApp.app;
 
   await db.dropDatabase();
 });
 
 afterAll(async () => {
   await db.close();
-  server.close();
 });
 
 describe('GET /posts/:slug', () => {
   it('Should return status 404 and a message for non-existing slug', async () => {
     const response = await request(app).get('/api/posts/non-existing-slug');
 
-    expect(response.status).toEqual(404);
+    expect(response.status).toBe(404);
     expect(response.body.error).toMatchObject({
       message: "Post doesn't exist",
+    });
+  });
+
+  // TODO: Temporary implementation
+  it('Should return status 200 and a post with its author if a post by provided slug exists', async () => {
+    const user = await User.create({
+      login: 'test',
+      email: 'test@gmail.com',
+      password: 'test',
+      salt: 'test',
+      hash: 'test',
+    });
+
+    const post = await Post.create({
+      title: 'My post title',
+      slug: 'my-post-title-d2k5g8',
+      author: user._id,
+    });
+
+    const response = await request(app).get(`/api/posts/${post.slug}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      title: post.title,
+      slug: post.slug,
+      author: {
+        login: user.login,
+        avatar: '',
+      },
+      sections: [],
+      commentCount: 0,
+      rating: 0,
+      createdAt: expect.any(String),
+      tags: [],
     });
   });
 });
