bcb9b164df2ae13f6e0c33f4f69509f2fc5abc0a
Darkzarich <darkzarichv2@gmail.com>
2024-07-28 20:09:52 +0300

    backend error responses tests for endpoint for getting author's posts

---
+++ server/src/controllers/posts/get-list-by-author.spec.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const { startApp } = require('../../../worker');
+const { connectDB } = require('../../../db');
+
+let app;
+let db;
+
+beforeAll(async () => {
+  db = await connectDB();
+
+  const resolvedApp = await startApp({ db });
+
+  app = resolvedApp.app;
+
+  await db.dropDatabase();
+});
+
+afterAll(async () => {
+  await db.close();
+});
+
+describe('GET /posts?author=', () => {
+  it('Should return status 422 and an expected error message for limit > 100', async () => {
+    const response = await request(app).get(
+      '/api/posts?author=some-author&limit=101',
+    );
+
+    expect(response.status).toBe(422);
+    expect(response.body.error).toMatchObject({
+      message: "Limit can't be more than 100",
+    });
+  });
+
+  it("Should return status 404 and an expected error message if author doesn't exist", async () => {
+    const response = await request(app).get(
+      '/api/posts?author=not-existing-author',
+    );
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toMatchObject({
+      message: "Author doesn't exist",
+    });
+  });
+});
