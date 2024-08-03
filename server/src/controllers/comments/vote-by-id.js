ebfa01622bf5110f577679099be31c907cb55cad
Darkzarich <darkzarichv2@gmail.com>
2024-08-03 14:37:00 +0300

    backend refactor comments controllers to return new class errors & async\await logic instead of promises

--- server/src/controllers/comments/vote-by-id.js
+++ server/src/controllers/comments/vote-by-id.js
@@ -2,59 +2,51 @@ const User = require('../../models/User');
 const Rate = require('../../models/Rate');
 const Comment = require('../../models/Comment');
 const { COMMENT_RATE_VALUE } = require('../../constants');
-const { success, generateError } = require('../../utils/utils');
+const { ForbiddenError, NotFoundError } = require('../../errors');
+const { success } = require('../../utils/utils');
 
-exports.voteById = async (req, res, next) => {
+exports.voteById = async (req, res) => {
   const { userId } = req.session;
   const { id } = req.params;
   const { negative } = req.body;
 
-  const foundComment = await Comment.findById(id);
+  const targetComment = await Comment.findById(id);
 
-  if (foundComment) {
-    if (foundComment.author._id.toString() === userId) {
-      generateError("Can't rate your own comment", 403, next);
-      return;
-    }
-
-    const user = await User.findById(userId).populate('rates');
-    const rated = user.isRated(foundComment.id);
-
-    if (!rated.result) {
-      foundComment.rating += negative
-        ? -COMMENT_RATE_VALUE
-        : COMMENT_RATE_VALUE;
-
-      Promise.all([
-        Rate.create({
-          target: foundComment.id,
-          targetModel: 'Comment',
-          negative,
-        }),
-        foundComment.save(),
-        User.findById(foundComment.author),
-      ])
-        .then((result) => {
-          const newRate = result[0];
-          const commentAuthor = result[2];
-
-          user.rates.push(newRate._id);
-          commentAuthor.rating += negative
-            ? -COMMENT_RATE_VALUE
-            : COMMENT_RATE_VALUE;
-
-          user.save();
-          commentAuthor.save();
-
-          success(req, res);
-        })
-        .catch((e) => {
-          next(e);
-        });
-    } else {
-      generateError("Can't rate comment you already rated", 403, next);
-    }
-  } else {
-    generateError("Comment doesn't exist", 404, next);
+  if (!targetComment) {
+    throw new NotFoundError('Comment does not exist');
   }
+
+  if (targetComment.author._id.toString() === userId) {
+    throw new ForbiddenError('Cannot rate your own comment');
+  }
+
+  const currentUser = await User.findById(userId).populate('rates');
+
+  const ratedForCurrentUser = currentUser.isRated(targetComment.id);
+
+  if (ratedForCurrentUser.result) {
+    throw new ForbiddenError('Cannot rate a comment you have already rated');
+  }
+
+  const rateValue = negative ? -COMMENT_RATE_VALUE : COMMENT_RATE_VALUE;
+
+  const newRate = await Rate.create({
+    target: targetComment.id,
+    targetModel: 'Comment',
+    negative,
+  });
+
+  await Promise.all([
+    User.updateOne({ _id: currentUser.id }, { $push: { rates: newRate.id } }),
+    User.updateOne(
+      { _id: targetComment.author },
+      { $inc: { rating: rateValue } },
+    ),
+    Comment.updateOne(
+      { _id: targetComment.id },
+      { $inc: { rating: rateValue } },
+    ),
+  ]);
+
+  success(req, res);
 };
