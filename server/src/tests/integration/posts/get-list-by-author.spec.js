ced75c5f302a9a6a41e5f3325302473d374c4d89
Darkzarich <darkzarichv2@gmail.com>
2024-11-24 16:28:48 +0300

    backend integration tests for GET /posts?author= endpoint

--- server/src/tests/integration/posts/get-list-by-author.spec.js
+++ server/src/tests/integration/posts/get-list-by-author.spec.js
@@ -1,6 +1,12 @@
 import request from 'supertest';
 import App from '../../../app.js';
 import { connectDB } from '../../../libs/db.js';
+import Post from '../../../models/Post.js';
+import User from '../../../models/User.js';
+import {
+  generateRandomPost,
+  generateRandomUser,
+} from '../../data-generators/index.js';
 
 let app;
 let db;
@@ -38,5 +44,62 @@ describe('GET /posts?author=', () => {
 
     expect(response.body.error.message).toBe("Author doesn't exist");
     expect(response.status).toBe(404);
+  });
+
+  it("Should return empty list of posts if author doesn't have any posts", async () => {
+    const otherUser = await User.create(generateRandomUser());
+
+    const response = await request(app).get(
+      `/api/posts?author=${otherUser.login}`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      posts: [],
+      total: 0,
+      pages: 0,
+      hasNextPage: false,
+    });
+  });
+
+  it("Should a list of the author's posts with an expected structure", async () => {
+    const otherUser = await User.create(generateRandomUser());
+
+    const post = (
+      await Post.create(
+        generateRandomPost({
+          author: otherUser._id,
+        }),
+      )
+    ).toJSON();
+
+    const response = await request(app).get(
+      `/api/posts?author=${otherUser.login}`,
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      posts: [
+        {
+          id: post._id.toString(),
+          title: post.title,
+          slug: post.slug,
+          author: {
+            id: otherUser._id.toString(),
+            login: otherUser.login,
+            avatar: otherUser.avatar,
+          },
+          sections: post.sections,
+          commentCount: 0,
+          rating: 0,
+          tags: post.tags,
+          rated: { isRated: false, negative: false },
+          createdAt: post.createdAt.toISOString(),
+        },
+      ],
+      total: 1,
+      pages: 1,
+      hasNextPage: false,
+    });
   });
 });
