30d77db071e6bc44aed1598991154b9261162f9b
Darkzarich <darkzarichv2@gmail.com>
2025-01-04 17:29:56 +0300

    backend use lean in comments/vote-by-id controller

--- packages/server/src/controllers/comments/vote-by-id.js
+++ packages/server/src/controllers/comments/vote-by-id.js
@@ -13,19 +13,21 @@ export async function voteById(req, res) {
   const targetComment = await Comment.findOne({
     _id: commentId,
     deleted: false,
-  }).select('author');
+  })
+    .select({ author: 1 })
+    .lean();
 
   if (!targetComment) {
     throw new NotFoundError(ERRORS.COMMENT_NOT_FOUND);
   }
 
-  if (targetComment.author._id.toString() === userId) {
+  if (targetComment.author.toString() === userId) {
     throw new ForbiddenError(ERRORS.COMMENT_CANT_RATE_OWN);
   }
 
   const currentUser = await User.findById(userId).populate('rates');
 
-  const ratedForCurrentUser = currentUser.isRated(targetComment.id);
+  const ratedForCurrentUser = currentUser.isRated(targetComment._id.toString());
 
   if (ratedForCurrentUser.result) {
     throw new ForbiddenError(ERRORS.COMMENT_CANT_RATE_ALREADY_RATED);
@@ -34,14 +36,14 @@ export async function voteById(req, res) {
   const rateValue = negative ? -COMMENT_RATE_VALUE : COMMENT_RATE_VALUE;
 
   const newRate = await Rate.create({
-    target: targetComment.id,
+    target: targetComment._id,
     targetModel: 'Comment',
     negative,
   });
 
   const [updatedComment] = await Promise.all([
     Comment.findByIdAndUpdate(
-      targetComment.id,
+      targetComment._id,
       { $inc: { rating: rateValue } },
       { new: true, lean: true },
     ),
