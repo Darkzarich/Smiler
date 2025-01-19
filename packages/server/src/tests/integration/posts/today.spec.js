0f41eb756d67f9c23b2a88d0130945966215cf04
Darkzarich <darkzarichv2@gmail.com>
2025-01-19 18:24:10 +0300

    backend integration tests for GET /posts/categories/today

---
+++ packages/server/src/tests/integration/posts/today.spec.js
@@ -0,0 +1,244 @@
+import request from 'supertest';
+import { subDays, subSeconds } from 'date-fns';
+import { signUpRequest } from '../../utils/request-auth.js';
+import Post from '../../../models/Post.js';
+import User from '../../../models/User.js';
+import Rate from '../../../models/Rate.js';
+import {
+  generateRandomPost,
+  generateRandomUser,
+  generateRate,
+} from '../../data-generators/index.js';
+import { ERRORS } from '../../../errors/index.js';
+
+describe('GET /posts/categories/today', () => {
+  it('Should return status 422 and an expected message for limit greater than 15', async () => {
+    const { sessionCookie } = await signUpRequest(global.app);
+
+    const response = await request(global.app)
+      .get('/api/posts/categories/today?limit=16')
+      .set('Cookie', sessionCookie);
+
+    expect(response.body.error.message).toBe(ERRORS.POST_LIMIT_PARAM_EXCEEDED);
+    expect(response.status).toBe(422);
+  });
+
+  it('Should return empty list of posts if there are no posts', async () => {
+    const { sessionCookie } = await signUpRequest(global.app);
+
+    const response = await request(global.app)
+      .get('/api/posts/categories/today')
+      .set('Cookie', sessionCookie);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      posts: [],
+      total: 0,
+      pages: 0,
+      hasNextPage: false,
+    });
+  });
+
+  it('Should return empty list of posts if are posts but posted not today', async () => {
+    const { sessionCookie } = await signUpRequest(global.app);
+
+    await Post.create(
+      generateRandomPost({
+        createdAt: subDays(new Date(), 2).toISOString(),
+      }),
+    );
+
+    const response = await request(global.app)
+      .get('/api/posts/categories/today')
+      .set('Cookie', sessionCookie);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      posts: [],
+      total: 0,
+      pages: 0,
+      hasNextPage: false,
+    });
+  });
+
+  it('Should return list of posts with the expected structure if there are posts that fit the criteria', async () => {
+    const otherUser = await User.create(generateRandomUser());
+
+    const post = await Post.create(
+      generateRandomPost({
+        author: otherUser._id,
+      }),
+    );
+
+    const response = await request(global.app).get(
+      '/api/posts/categories/today',
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      posts: [
+        {
+          id: post._id.toString(),
+          title: post.title,
+          slug: post.slug,
+          author: {
+            id: otherUser._id.toString(),
+            login: otherUser.login,
+            avatar: otherUser.avatar,
+          },
+          sections: post.sections.toObject(),
+          commentCount: 0,
+          rating: post.rating,
+          tags: post.tags.toObject(),
+          rated: { isRated: false, negative: false },
+          createdAt: post.createdAt.toISOString(),
+        },
+      ],
+      total: 1,
+      pages: 1,
+      hasNextPage: false,
+    });
+  });
+
+  it('Should return more than one page in pagination if there are more than limit posts existing', async () => {
+    const posts = Array(11)
+      .fill({})
+      .map((_, index) =>
+        generateRandomPost({
+          slug: `slug${index}`,
+        }),
+      );
+
+    await Post.insertMany(posts);
+
+    const response = await request(global.app).get(
+      '/api/posts/categories/today?limit=10',
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body.posts).toHaveLength(10);
+    expect(response.body).toMatchObject({
+      total: 11,
+      pages: 2,
+      hasNextPage: true,
+    });
+  });
+
+  it('Should correctly offset pagination', async () => {
+    const posts = Array(11)
+      .fill({})
+      .map((_, index) =>
+        generateRandomPost({
+          slug: `slug${index}`,
+        }),
+      );
+
+    await Post.insertMany(posts);
+
+    const response = await request(global.app).get(
+      '/api/posts/categories/today?limit=10&offset=10',
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body.posts).toHaveLength(1);
+    expect(response.body).toMatchObject({
+      total: 11,
+      pages: 2,
+      hasNextPage: false,
+    });
+  });
+
+  it('Should return posts as rated if user rated them', async () => {
+    const { sessionCookie, currentUser } = await signUpRequest(global.app);
+
+    const otherUser = await User.create(generateRandomUser());
+
+    const post1 = await Post.create(
+      generateRandomPost({
+        author: otherUser._id,
+      }),
+    );
+
+    const post2 = await Post.create(
+      generateRandomPost({
+        author: otherUser._id,
+      }),
+    );
+
+    const rate1 = await Rate.create(
+      generateRate({
+        target: post1._id,
+        negative: true,
+        targetModel: 'Post',
+      }),
+    );
+
+    const rate2 = await Rate.create(
+      generateRate({
+        target: post2._id,
+        negative: false,
+        targetModel: 'Post',
+      }),
+    );
+
+    await User.findByIdAndUpdate(currentUser.id, {
+      $push: { rates: { $each: [rate1._id, rate2._id] } },
+    });
+
+    const response = await request(global.app)
+      .get('/api/posts/categories/today')
+      .set('Cookie', sessionCookie);
+
+    expect(response.status).toBe(200);
+    expect(response.body.posts[0]).toMatchObject({
+      rated: {
+        isRated: true,
+        negative: true,
+      },
+    });
+    expect(response.body.posts[1]).toMatchObject({
+      rated: {
+        isRated: true,
+        negative: false,
+      },
+    });
+  });
+
+  it('Should sort posts by rating descending', async () => {
+    const dates = [
+      subSeconds(new Date(), 1).toISOString(),
+      subSeconds(new Date(), 2).toISOString(),
+      subSeconds(new Date(), 3).toISOString(),
+    ];
+
+    await Promise.all([
+      Post.create(
+        generateRandomPost({
+          createdAt: dates[2],
+          rating: 1,
+        }),
+      ),
+      Post.create(
+        generateRandomPost({
+          createdAt: dates[1],
+          rating: 0,
+        }),
+      ),
+      Post.create(
+        generateRandomPost({
+          createdAt: dates[0],
+          rating: 2,
+        }),
+      ),
+    ]);
+
+    const response = await request(global.app).get(
+      '/api/posts/categories/today',
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body.posts[0].rating).toBe(2);
+    expect(response.body.posts[1].rating).toBe(1);
+    expect(response.body.posts[2].rating).toBe(0);
+  });
+});
