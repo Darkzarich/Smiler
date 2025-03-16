e26ca6d66b0424ab8f9c9f81ce7c1dab589bf88f
Darkzarich <darkzarichv2@gmail.com>
2025-03-16 19:18:38 +0300

    backend change swagger from building spec before running to building spec during runtime

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,19 +1,34 @@
 import express from 'express';
-import { readFile } from 'node:fs/promises';
-import { serve, setup } from 'swagger-ui-express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import apiRoutes from './api/index';
 import Config from '../config/index';
 
 const router = express.Router();
 
 if (!Config.IS_JEST) {
-  const buffer = await readFile(
-    new URL('../docs/swagger/swagger.json', import.meta.url),
-  );
+  const swaggerSpec = swaggerJSDoc({
+    openapi: '3.0.0',
+    info: {
+      title: 'Smiler Api', // Title (required)
+      version: '1.0.0', // Version (required)
+      // Description (optional)
+      description:
+        'Smiler is my own MEVN (MongoDB, Express, Vue.js, Node.js) site similar to reddit.com or 9gag.com (mostly takes many known features) with many different and awesome features, open Swagger API docs, tests, interesting tools and more. Main reason of making this site is fun and learning new things while making it',
+    },
+    servers: [
+      {
+        url: 'https://smiler-api.darkzarich.com/api',
+        description: 'Production server',
+      },
+      {
+        url: 'http://localhost:3000/api',
+        description: 'Local server',
+      },
+    ],
+  });
 
-  const swaggerDocument = JSON.parse(buffer.toString());
-
-  router.use('/api-docs', serve, setup(swaggerDocument));
+  router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
 router.use('/api', apiRoutes);
